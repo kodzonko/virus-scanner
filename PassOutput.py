71fd8703f82d0455dde764ba7c2b755a4aa15318
Jan Wadolowski <janwadolowski@gmail.com>
2020-07-01 01:36:30 +0200

    File structure and program workflow draft

---
+++ PassOutput.py
@@ -0,0 +1,12 @@
+class PassOutput:
+    def print_to_console():
+        pass
+
+    def write_to_file(target_file):
+        pass
+
+    if print_output == True:
+        pass
+
+    if write_output == True:
+        pass
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import requests
-import json
+# !/usr/bin/python
+
 import pprint
-import sys
+
+import requests
 
 scan_url = 'https://www.virustotal.com/vtapi/v2/file/scan'
-scan_params = {'apikey': f'{api-key}'}
+scan_params = {'apikey': f'{api - key}'}
 
-#test_file = input("Proszę podać ścieżkę do pliku: ")
+# test_file = input("Proszę podać ścieżkę do pliku: ")
 
 test_file = "kod_z_zajec.py"
 files = {'file': (test_file, open(test_file, 'rb'))}
@@ -19,6 +20,6 @@ if scan_request.status_code != 200:
     print(f"Coś się popsuło, brak wyników. [{scan_request.status_code}]")
 else:
     report_url = 'https://www.virustotal.com/vtapi/v2/file/report'
-    report_params = {'apikey': f'{api-key}', 'resource': scan_id, 'allinfo': True}
+    report_params = {'apikey': f'{api - key}', 'resource': scan_id, 'allinfo': True}
     report_response = requests.get(report_url, params=report_params)
     pprint.pprint(report_response.json())
