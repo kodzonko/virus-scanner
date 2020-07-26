5d78647b38e634f496f7eb53f2ad6b019e07e4da
Jan Wadolowski <janwadolowski@gmail.com>
2020-07-26 23:15:07 +0200

    file handler miner edits

--- FileHandler.py
+++ FileHandler.py
@@ -3,16 +3,30 @@ from tkinter import Tk, filedialog
 
 class FileHandler:
 
+    files_to_scan = []
     @classmethod
-    def ask_for_path(cls):
+    def select_files_to_scan(cls):
         """
         Opens minimal GUI to navigate and select file(s) of choice
         :return:
         Path (str) to the chosen file(s)
         """
-        file_picker_window = Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
-        file_picker_window.lift()
-        file_picker_window.focus_force()
+        selected_files = Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+        selected_files.lift()
+        selected_files.focus_force()
 
-        file_picker_window.file_name = filedialog.askopenfilenames(initialdir="C:", title="Select file(s)")
-        return [file_picker_window]
+        selected_files.file_name = filedialog.askopenfilenames(initialdir="C:", title="Select file(s) to scan")
+        cls.files_to_scan.extend(selected_files)
+
+    def select_API_file(cls):
+        """
+        Opens minimal GUI to navigate and select file(s) of choice
+        :return:
+        Path (str) to the chosen file(s)
+        """
+        selected_files = Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+        selected_files.lift()
+        selected_files.focus_force()
+
+        selected_files.file_name = filedialog.askopenfilenames(initialdir="C:", title="Select a file containing your API key")
+        cls.files_to_scan.extend(selected_files)
